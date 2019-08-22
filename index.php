7cbdec8bdb921f4464bc5f6ac8d39d9b097f8a7e
Coron3l <49570478+Coron3l@users.noreply.github.com>
2019-08-22 20:04:59 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <meta charset="utf-8">  
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    </head>
+    <body>
+        <div class="container" style="color: white;"> 
+            <div class="col-md-5 border"    style="margin-top: 150px; background: black;">
+                <form>
+                    <p><h3>Acessar Sistema</h3></p>
+                    <div class="form-group">
+                        <input type="email" name="HTML_email" placeholder="Digite seu E-mail" class="form-control"> 
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="HTML_senha" placeholder="Entre com a senha" class="form-control">
+                    </div>
+                <div class="form-group" style="margin-left: 248px;">
+                        <button type="submit" class="btn btn-outline-light"> Entrar </button>
+                        <button class="btn btn-outline-light"><a href="src/UI/FormCadastroUsuarioUI.php"> Cadastre-se </a></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
